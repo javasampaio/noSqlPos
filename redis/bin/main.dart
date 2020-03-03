edf7254fd7800866dbda230e0decb01beac8ba5a
leandro.sampaio <leandro.sampaio@hbsis.com.br>
2020-03-03 09:45:17 -0300

    insert data in redis

--- redis/bin/main.dart
+++ redis/bin/main.dart
@@ -4,66 +4,115 @@ RedisConnection connection;
 const String HOST = "localhost";
 const int PORT = 6379;
 
-const String userKey = "user:";
-const String nameKey = "name ";
-const String cartelaKey = "bcartela ";
-const String scoreKey = "bscore ";
+const String USER_KEY = "user:";
+const String NAME_KEY = "name";
+const String CARTELA_KEY = "bcartela";
+const String SCORE_KEY = "bscore";
+
+const String NUMBERS = "numbers";
+const String BAG_NUMBERS = "bagNumbers";
+
+const int TOTAL_PLAYERS = 2; //O bingo será jogado por 50 pessoas.
+const int TOTAL_CARTELA_NUMBERS = 15;
+
+const String CARTELA = "cartela";
+const String SCORE = "score";
 
 Future<void> main() async {
   connection = RedisConnection();
-  print("Inicio Bingo");
-  await initBingo();
-  var cartela = await getCartela();
-  print("aki");
-  print(cartela);
-  //setPlayers(1);
-  //jogado por 50 pessoas
+  print("start config Redinsgo");
+  await generateNumbers(NUMBERS); //utilize um set com números de 1 a 99
+  await generateCartelas(); //utilize sets com 15 números aleatórios cada
+  await generateScore(); //utilize uma estrutura de set score para controlar a pontuação de cada participante
+  await setPlayers(); //O bingo será jogado por 50 pessoas.
+  await generateNumbers(BAG_NUMBERS); //crie um set para ter as “pedras”
+  await play(); //crie um “jogo” e retire uma a uma
 
-  print("Fim Bingo");
+  print("finish Redinsgo");
 }
 
-void setPlayers(int total) {
-  connection.connect(HOST, PORT).then((Command command) {
-    for (var i = 0; i < total; ++i) {
-      var user = userKey + i.toString();
-      var cartela = cartelaKey + i.toString();
-      var score = scoreKey + i.toString();
+Future<void> setPlayers() async {
+  await connection.connect(HOST, PORT).then((Command command) async{
+    for (var i = 0; i < TOTAL_PLAYERS; ++i) {
+      var user = "${USER_KEY}:${i}";
+      var cartela = "${CARTELA}:${i}";
+      var score = "${SCORE}:${i}";
 
-      command.send_object(["HSET", user, "${nameKey} ${userKey}"]).catchError(
+      await command.send_object(["HSET", user, "${NAME_KEY} ${user}"]).catchError(
           (onError) {
         print(onError);
       });
-      command.send_object(
-          ["HSET", user, "${cartelaKey} ${cartela}"]).catchError((onError) {
+      await command.send_object(
+          ["HSET", user, "${CARTELA_KEY} ${cartela}"]).catchError((onError) {
         print(onError);
       });
-      command.send_object(["HSET", user, "${scoreKey} ${score}"]).catchError(
+      await command.send_object(["HSET", user, "${SCORE_KEY} ${score}"]).catchError(
           (onError) {
         print(onError);
       });
     }
   });
+  connection.close();
 }
 
-Future<dynamic> getCartela() async {
-  await connection.connect(HOST, PORT).then((Command command) {
-    command.send_object(["SRANDMEMBER", "numbers"]).then((onValue) {
-      print(onValue);
-      return onValue;
-    }).catchError((error) {
-      print(error);
-    });
+Future<void> generateCartelas() async {
+  await connection.connect(HOST, PORT).then((Command command) async {
+    for (var i = 1; i <= TOTAL_PLAYERS; i++) {
+      var cartela = "${CARTELA}:${i}";
+      var isFull = false;
+      while (!isFull) {
+        await command
+            .send_object(["SRANDMEMBER", NUMBERS]).then((randonNumber) async {
+          await command.send_object(["SCARD", cartela]).then((size) async {
+            if (size == TOTAL_CARTELA_NUMBERS) {
+              isFull = true;
+            } else {
+              await command
+                  .send_object(["SADD", cartela, randonNumber])
+                  .catchError((error) {
+                    print(error);
+                  });
+            }
+          }).catchError((error) {
+            print(error);
+          });
+        }).catchError((error) {
+          print(error);
+        });
+      }
+    }
   });
+  connection.close();
 }
 
-Future<void> initBingo() async {
-  for (var i = 1; i < 100; i++) {
-    await connection.connect(HOST, PORT).then((Command command) {
-      command.send_object(["SADD", "numbers", "${i}"]).then((response) {
-        print(response);
-      }).catchError((onError) {
+Future<void> generateScore() async {
+  await connection.connect(HOST, PORT).then((Command command) async {
+    for (var i = 0; i < TOTAL_PLAYERS; ++i) {
+       var score = "${SCORE}:${i}";
+       await command
+                  .send_object(["SET", score, 0])
+                  .catchError((error) {
+                    print(error);
+                  });
+    }
+  });
+  connection.close();
+}
+
+Future<void> generateNumbers(String key) async {
+  await connection.connect(HOST, PORT).then((Command command) async {
+    for (var i = 1; i < 100; i++) {
+      await command
+          .send_object(["SADD", key, "${i}"]).catchError((onError) {
         print(onError);
       });
-    });
-  }
+    }
+  });
+  connection.close();
+}
+
+Future<void> play() async {
+  await connection.connect(HOST, PORT).then((Command command) async {
+
+  });
 }
